aa7e05203e15ac6068ce8fa8e2a832a68adc7d48
Basavaraj <basav@gmail.com>
2022-09-19 11:12:15 +0000

    added a comment to make the pgm readable

--- sales.java
+++ sales.java
@@ -4,5 +4,6 @@ class Sales {
 		System.out.println("sales ingestion has started get ready");
 		System.out.println("first program done");
 		System.out.println("git diff trial");
+		//this is a comment
 	}
 }

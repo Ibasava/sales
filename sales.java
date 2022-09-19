d5e5cdcfd63e7341db75a1390584e5ddfae01d39
Basavaraj <basav@gmail.com>
2022-09-19 11:17:49 +0000

    adding previous version

--- sales.java
+++ sales.java
@@ -4,6 +4,5 @@ class Sales {
 		System.out.println("sales ingestion has started get ready");
 		System.out.println("first program done");
 		System.out.println("git diff trial");
-		//this is a comment
 	}
 }
